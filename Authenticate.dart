240bdb4c394a120ea1493ef63273dec108e2f386
Abhisolanki16 <97236636+Abhisolanki16@users.noreply.github.com>
2022-01-28 12:24:42 +0530

    Add files via upload

---
+++ Authenticate.dart
@@ -0,0 +1,17 @@
+import 'HomeScreen.dart';
+import 'LoginScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_auth.currentUser != null) {
+      return HomeScreen();
+    } else {
+      return LoginScreen();
+    }
+  }
+}
---
+++ DatabaseManager.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+
+class DatabaseManager{
+  final CollectionReference profileList = FirebaseFirestore.instance.collection('profileInfo');
+
+  Future createUserData(String name, String email, String uid) async{
+    return await profileList.doc(uid).set({
+      'name': name,
+      'email': email,
+    });
+  }
+
+  Future getUsersList() async{
+    List itemList = [];
+    try{
+      await profileList.get().then((QuerySnapshot){
+        QuerySnapshot.docs.forEach((element) {
+          itemList.add(element.data());
+        
+    });
+    });
+      return itemList;
+    }
+    catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+}
